4845f732a7ff70978617ef6f87dcefb15bd043ea
saimohan4356 <89723963+saimohan4356@users.noreply.github.com>
2021-09-08 16:09:54 +0530

    Add files via upload

---
+++ 4_Implementation/SRC/function.h
@@ -0,0 +1,40 @@
+/**
+ * @file snakefunctions.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __FUNCTIONS_H__
+#define __FUNCTIONS_H__
+
+void setup();
+void  draw();
+
+
+void =input();
+void logic();
+void Food();
+int Score();
+void Print();
+void Down();
+void Left();
+void Up();
+void Right();
+void ExitGame();
+int Score();
+
+int x,y;
+int fruitx;
+int fruity;
+int pause;
+int tailx;
+int taily;
+int counttail;
+
+
+#endif
